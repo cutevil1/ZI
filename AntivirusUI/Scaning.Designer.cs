c7277173e3e5806af780c1af9c4c4f7740e8e19c
cutevil1 <vladlenaa811@gmail.com>
2020-06-14 22:41:22 +0300

    bug fix

--- AntivirusUI/Scaning.Designer.cs
+++ AntivirusUI/Scaning.Designer.cs
@@ -2,15 +2,10 @@
 {
     partial class Scaning
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
+       
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+       
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +17,6 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.Break_button = new System.Windows.Forms.Button();
@@ -39,9 +30,7 @@
             this.start_stop_button = new System.Windows.Forms.Button();
             this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // Break_button
-            // 
+           
             this.Break_button.BackColor = System.Drawing.Color.White;
             this.Break_button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.Break_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
@@ -54,9 +43,7 @@
             this.Break_button.Text = "Стоп";
             this.Break_button.UseVisualStyleBackColor = false;
             this.Break_button.Click += new System.EventHandler(this.Break_button_Click);
-            // 
-            // label1
-            // 
+           
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.ForeColor = System.Drawing.Color.Black;
@@ -66,9 +53,7 @@
             this.label1.Size = new System.Drawing.Size(73, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "Статус:";
-            // 
-            // label2
-            // 
+          
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.ForeColor = System.Drawing.Color.Black;
@@ -78,9 +63,7 @@
             this.label2.Size = new System.Drawing.Size(185, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "Количество файлов:";
-            // 
-            // label3
-            // 
+          
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label3.ForeColor = System.Drawing.Color.Black;
@@ -90,9 +73,7 @@
             this.label3.Size = new System.Drawing.Size(18, 20);
             this.label3.TabIndex = 4;
             this.label3.Text = "0";
-            // 
-            // label4
-            // 
+           
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label4.ForeColor = System.Drawing.Color.Black;
@@ -102,9 +83,7 @@
             this.label4.Size = new System.Drawing.Size(74, 20);
             this.label4.TabIndex = 5;
             this.label4.Text = "Угрозы:";
-            // 
-            // label5
-            // 
+            
             this.label5.AutoSize = true;
             this.label5.ForeColor = System.Drawing.Color.Black;
             this.label5.Location = new System.Drawing.Point(80, 71);
@@ -113,9 +92,7 @@
             this.label5.Size = new System.Drawing.Size(18, 20);
             this.label5.TabIndex = 6;
             this.label5.Text = "0";
-            // 
-            // label6
-            // 
+            
             this.label6.AutoSize = true;
             this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label6.Location = new System.Drawing.Point(137, 71);
@@ -124,9 +101,7 @@
             this.label6.Size = new System.Drawing.Size(18, 20);
             this.label6.TabIndex = 7;
             this.label6.Text = "0";
-            // 
-            // label7
-            // 
+            
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label7.ForeColor = System.Drawing.Color.Black;
@@ -136,9 +111,7 @@
             this.label7.Size = new System.Drawing.Size(175, 20);
             this.label7.TabIndex = 8;
             this.label7.Text = "Время выполнения:";
-            // 
-            // start_stop_button
-            // 
+           
             this.start_stop_button.BackColor = System.Drawing.Color.White;
             this.start_stop_button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.start_stop_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
@@ -151,9 +124,7 @@
             this.start_stop_button.Text = "Старт";
             this.start_stop_button.UseVisualStyleBackColor = false;
             this.start_stop_button.Click += new System.EventHandler(this.start_stop_button_Click);
-            // 
-            // label8
-            // 
+            
             this.label8.AutoSize = true;
             this.label8.BackColor = System.Drawing.Color.White;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
@@ -163,9 +134,7 @@
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(0, 20);
             this.label8.TabIndex = 10;
-            // 
-            // Scaning
-            // 
+            
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
